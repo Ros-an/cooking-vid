7f3a13cbd286dd18b735400e03425e5bb8a36436
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-11 18:46:37 +0530

    create header component

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <img
+        src="https://github.com/Ros-an/cooking-vid/blob/development/src/logo.png"
+        alt="logo"
+      />
+      <h1>Hi i am header</h1>
+    </div>
+  );
+}
+
+export default Header;
