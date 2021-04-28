6899763340022c699af55b879c4bc0f4be679eed
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-28 12:53:38 +0530

    added: search and logo

--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,23 @@
 import React from "react";
+import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
+import SearchIcon from "@material-ui/icons/Search";
 
 function Header() {
   return (
     <div className="header">
-      <img
-        src="https://github.com/Ros-an/cooking-vid/blob/development/src/logo.png"
-        alt="logo"
-      />
-      <h1>Hi i am header</h1>
+      <button className="header__logo">
+        <span>
+          C<span className="ooking">ooking</span> Vid
+        </span>
+        <PlayCircleFilledIcon style={{ fontSize: "1.5rem" }} />
+      </button>
+      <div className="search-section">
+        <input type="text" />
+        <button className="search-icon">
+          <SearchIcon />
+        </button>
+      </div>
+      <p className="login">Login</p>
     </div>
   );
 }
