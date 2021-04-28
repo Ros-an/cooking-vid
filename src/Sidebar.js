d8e0a68e1b5a4d5242f83c0975476b6aef7d4e15
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-04-28 12:53:58 +0530

    dummy sidebar

---
+++ src/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__sec">one</div>
+      <div className="sidebar__sec">Two</div>
+      <div className="sidebar__sec">Three</div>
+      <div className="sidebar__sec">Four</div>
+    </div>
+  );
+}
+
+export default Sidebar;
