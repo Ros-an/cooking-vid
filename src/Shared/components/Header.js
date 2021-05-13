78137aafc08bf0c28d03959ea7b0acbc27ae5758
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-13 14:01:51 +0530

    refactor: link

--- src/Shared/components/Header.js
+++ src/Shared/components/Header.js
@@ -2,31 +2,36 @@ import React from "react";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
 import SearchIcon from "@material-ui/icons/Search";
 import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
+import { NavLink } from "react-router-dom";
 
 import "./Header.css";
 
 function Header() {
   return (
     <div className="header">
-      <button className="header__logo">
-        <span className="logoText">
-          C<span className="ooking">ooking</span> Vid
-        </span>
-        <PlayCircleFilledIcon style={{ fontSize: "1.5rem" }} />
-      </button>
+      <NavLink to="/">
+        <button className="header__logo">
+          <span className="logoText">
+            C<span className="ooking">ooking</span> Vid
+          </span>
+          <PlayCircleFilledIcon style={{ fontSize: "1.5rem" }} />
+        </button>
+      </NavLink>
       <div className="search-section">
         <input type="text" placeholder="Search" />
         <button className="search-icon">
           <SearchIcon className="search-icon" />
         </button>
       </div>
-      <p className="login">
-        {" "}
-        <span className="login-item__text">Login/SignUp</span>
-        <span className="login-item__logo">
-          <AccountCircleRoundedIcon style={{ fontSize: "1.75rem" }} />
-        </span>
-      </p>
+      <NavLink to="/authenticate">
+        <p className="login">
+          {" "}
+          <span className="login-item__text">Login/SignUp</span>
+          <span className="login-item__logo">
+            <AccountCircleRoundedIcon style={{ fontSize: "1.75rem" }} />
+          </span>
+        </p>
+      </NavLink>
     </div>
   );
 }
