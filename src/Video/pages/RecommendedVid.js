af2bfc2bef968ac74a65e71e862ee321029f08a3
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-13 14:05:02 +0530

    refactor: prop passed to videocard

--- src/Video/pages/RecommendedVid.js
+++ src/Video/pages/RecommendedVid.js
@@ -2,14 +2,15 @@ import React from "react";
 import VideoCard from "../components/VideoCard";
 
 import "./RecommendedVid.css";
+import { videoData } from "../data";
 
 function RecommendedVid() {
   return (
     <div className="recommended-vid">
       <h2>Recommended</h2>
       <div className="recommended-vid__videos">
-        {[1, 1, 1, 1, 1, 1, 11, 1, 1, 1, 1, 1].map((item) => (
-          <VideoCard />
+        {videoData.map((item) => (
+          <VideoCard videoInfo={item} />
         ))}
       </div>
     </div>
