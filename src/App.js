fc2d792679b10fb263bd3d157528e97e26d0de6d
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-13 14:01:00 +0530

    added: authenticate component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,26 @@ import React from "react";
 import Header from "./Shared/components/Header";
 import Sidebar from "./Shared/components/Sidebar";
 import RecommendedVid from "./Video/pages/RecommendedVid";
+import SignUpSignIn from "./Pages/SignUpSignIn";
 
 import { Routes, Route } from "react-router-dom";
 
+function Home() {
+  return (
+    <>
+      <Sidebar />
+      <RecommendedVid />
+    </>
+  );
+}
 function App() {
   return (
     <div className="App">
       <Header />
-      <Sidebar />
       <main>
         <Routes>
-          <Route path="/roshan" element={<RecommendedVid />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/authenticate" element={<SignUpSignIn />} />
         </Routes>
       </main>
     </div>
