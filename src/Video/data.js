8e3f857c83c271e71d944dfb8ccdccf3845a463e
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-13 14:04:27 +0530

    curated data

---
+++ src/Video/data.js
@@ -0,0 +1,323 @@
+import { v4 as uuid } from "uuid";
+export const videoData = [
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKA | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year ago",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "JAPANESE STREET FOOD | Ramen Noodles",
+    views: "1.1M views",
+    timestamp: "2 years ago",
+    channel: "Siglex",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwnirmSwZVHeWSQefT2VZoKrxsUdhwFyh1lskwMlt=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/QHwTdOWARB0/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLB5K66AYRfvmG4pafY_U6O-ogJX2w",
+    gif:
+      "https://i.ytimg.com/an_webp/QHwTdOWARB0/mqdefault_6s.webp?du=3000&sqp=CPfA3YQG&rs=AOn4CLDy_AVFv4GmiZvSgV1VQdmQrCLN4g",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "JAPANESE FOOD | ICHIRAN Best Ramen in the World!",
+    views: "9.1M views",
+    timestamp: "2 years ago",
+    channel: "Travel Thirsty",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwnhF8JNVw8RjLzNB7cdIdm1qUKqLqt9OsMgP76K_JA=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/kpQC2DNX9K4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCYFhHTCe7Vfy6bTLmSMDXSEDBxyw",
+    gif:
+      "https://i.ytimg.com/an_webp/kpQC2DNX9K4/mqdefault_6s.webp?du=3000&sqp=CPCj3YQG&rs=AOn4CLCvjmi-Pju7N-6Jcb1RXrig5XuF4g",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "Famous Matka Biryani of Vadodara",
+    views: "245K views",
+    timestamp: "3 weeks ago",
+    channel: "The Foodie Nation",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwngXCX1Ir5Aj-iEBX216E3FyAaMcTPMBI3rdf2KccQ=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/FjmiJ7GHUy4/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCAZVIB3eNmKJmX23t2diq7-oQXmA",
+    gif:
+      "https://i.ytimg.com/an_webp/FjmiJ7GHUy4/mqdefault_6s.webp?du=3000&sqp=CNjD3YQG&rs=AOn4CLD1-cM8CBi-ujkn5n0qPJvd2wGeLw",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+  {
+    id: uuid(),
+    category: "",
+    title: "CHICKEN CHUKKS | Traditional Village Cooking",
+    views: "25M views",
+    timestamp: "1 year",
+    channel: "Village Cooking channel",
+    avatar_img:
+      "https://yt3.ggpht.com/ytc/AAUvwniE1RnIoH5mKPHwLuZzOAqe9VhqWO_pMYA9JZxkqw=s68-c-k-c0x00ffffff-no-rj",
+    image:
+      "https://i.ytimg.com/vi/Lhn32f7ymcA/hq720.jpg?sqp=-oaymwEcCOgCEMoBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCh0Xoh3_Rqp2hfHTUcDY5esgzt6w",
+    gif:
+      "https://i.ytimg.com/an_webp/Lhn32f7ymcA/mqdefault_6s.webp?du=3000&sqp=CICX3YQG&rs=AOn4CLBuW8K89IekkF2vNoiWzy4Td_x9IQ",
+
+    video: "https://youtube.video.link",
+  },
+];
