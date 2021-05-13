efc58a84f24f1c1574899213028410590ba3bdf6
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-13 14:02:58 +0530

    refactor: icons

--- src/Shared/components/Sidebar.js
+++ src/Shared/components/Sidebar.js
@@ -1,9 +1,9 @@
 import React from "react";
 import HomeIcon from "@material-ui/icons/Home";
-import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import ViewListIcon from "@material-ui/icons/ViewList";
 import HistoryIcon from "@material-ui/icons/History";
 import WatchLaterIcon from "@material-ui/icons/WatchLater";
-import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
 import StarsIcon from "@material-ui/icons/Stars";
@@ -13,10 +13,10 @@ import "./Sidebar.css";
 const sidebarTitle = [
   { id: 1, Icon: HomeIcon, title: "Home" },
   { id: 6, Icon: StarsIcon, title: "Recommended" },
-  { id: 2, Icon: VideoLibraryIcon, title: "Library" },
+  { id: 2, Icon: ViewListIcon, title: "Playlist" },
   { id: 3, Icon: HistoryIcon, title: "History" },
   { id: 4, Icon: WatchLaterIcon, title: "Watch Later" },
-  { id: 5, Icon: ThumbUpAltOutlinedIcon, title: "Liked One" },
+  { id: 5, Icon: ThumbUpIcon, title: "Liked One" },
 ];
 function Sidebar() {
   return (
