713f324d577ae19bd455790d79d12ffa493714af
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-20 15:33:51 +0530

    refactor: sidebar and recommenedVid component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from "react";
-import Header from "./Shared/components/Header";
-import Sidebar from "./Shared/components/Sidebar";
+import React, { useState } from "react";
+
+import Header from "./Shared/Header";
+import Sidebar from "./Shared/Sidebar";
 import RecommendedVid from "./Video/pages/RecommendedVid";
 import SignUpSignIn from "./Pages/SignUpSignIn";
+import PlayList from "./Pages/PlayList";
+import WatchLater from "./Pages/WatchLater";
+import History from "./Pages/History";
+import LikedOne from "./Pages/LikedOne";
 
 import { Routes, Route } from "react-router-dom";
 
-function Home() {
-  return (
-    <>
-      <Sidebar />
-      <RecommendedVid />
-    </>
-  );
-}
 function App() {
   return (
     <div className="App">
       <Header />
+      <Sidebar />
       <main>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<RecommendedVid />} />
+          <Route path="/playlist" element={<PlayList />} />
+          <Route path="/watchlater" element={<WatchLater />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/likedone" element={<LikedOne />} />
           <Route path="/authenticate" element={<SignUpSignIn />} />
         </Routes>
       </main>
