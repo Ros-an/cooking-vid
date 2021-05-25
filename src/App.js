72cc6b3966bed7bdd2c2c45ea65ab7c11ca375bd
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-25 16:26:50 +0530

    Route setup - 2 components

--- src/App.js
+++ src/App.js
@@ -8,21 +8,26 @@ import PlayList from "./Pages/PlayList";
 import WatchLater from "./Pages/WatchLater";
 import History from "./Pages/History";
 import LikedOne from "./Pages/LikedOne";
-
+import PlayListVideoList from "./Pages/PlayListVideoList";
+import { useAuthContext } from "./ContextAPI/auth-context";
+import VideoPage from "./Video/pages/VideoPage";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
+  const { authPage } = useAuthContext();
   return (
     <div className="App">
       <Header />
       <Sidebar />
-      <main>
+      <main className={`main-content ${authPage && "main-content-slide-off"}`}>
         <Routes>
           <Route path="/" element={<RecommendedVid />} />
+          <Route path="/:videoid" element={<VideoPage />} />
           <Route path="/playlist" element={<PlayList />} />
           <Route path="/watchlater" element={<WatchLater />} />
           <Route path="/history" element={<History />} />
           <Route path="/likedone" element={<LikedOne />} />
+          <Route path="/playlist/:name/:id" element={<PlayListVideoList />} />
           <Route path="/authenticate" element={<SignUpSignIn />} />
         </Routes>
       </main>
