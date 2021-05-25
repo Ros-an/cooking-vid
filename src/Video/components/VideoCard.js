891c13eca758eadc637c7cec3a404b5122b645ba
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-25 16:37:56 +0530

    refactoring

--- src/Video/components/VideoCard.js
+++ src/Video/components/VideoCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Avatar from "@material-ui/core/Avatar";
 import DropDownList from "./DropDownList";
-
+import { Link } from "react-router-dom";
 import PlayListModal from "../../Shared/PlayListModal";
 
 import "./VideoCard.css";
@@ -12,9 +12,15 @@ function VideoCard({ videoInfo }) {
   return (
     <>
       <PlayListModal />
-      <div className="video-card pointer-cursor" key={id}>
-        <img className="video-card__thumbnail static" src={image} alt={title} />
-        <img className="video-card__thumbnail active" src={gif} alt={title} />
+      <div className="video-card" key={id}>
+        <Link to={`/${id}`}>
+          <img
+            className="video-card__thumbnail static"
+            src={image}
+            alt={title}
+          />
+          <img className="video-card__thumbnail active" src={gif} alt={title} />
+        </Link>
         <div className="video-card__info">
           <Avatar alt={title} src={avatar_img} className="avatar" />
           <div className="video-card__text">
@@ -24,7 +30,7 @@ function VideoCard({ videoInfo }) {
               {views} • {timestamp}
             </p>
           </div>
-          <DropDownList />
+          <DropDownList videoInfo={videoInfo} />
         </div>
       </div>
     </>
