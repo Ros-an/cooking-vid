d7d6c4708d49932f6f488b04fe12d49dd0f6d181
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-05-25 16:37:14 +0530

    setup dropdown list

--- src/Video/components/DropDownList.js
+++ src/Video/components/DropDownList.js
@@ -1,17 +1,31 @@
 import React from "react";
 import { usePlayList } from "../../ContextAPI/playlist-context";
+import { useWatchLater } from "../../ContextAPI/watchlater-context";
 
-function DropDownList() {
+function DropDownList({ videoInfo }) {
   const { dispatchPlayList } = usePlayList();
+  const { dispatchWatchLater } = useWatchLater();
   return (
-    <span className="video-card__option dropdown">
+    <span className="video-card__option dropdown pointer-cursor">
       <i className="fa fa-ellipsis-v" aria-hidden="true"></i>
       <div className="dropdown-content">
-        <p onClick={() => dispatchPlayList({ type: "MODAL_CONTROL" })}>
+        <p
+          onClick={() =>
+            dispatchPlayList({ type: "MODAL_CONTROL", payload: videoInfo })
+          }
+        >
           Save to playlist
         </p>
-        <p>Save to watch Later</p>
-        <p>Share</p>
+        <p
+          onClick={() =>
+            dispatchWatchLater({
+              type: "ADD_TO_WATCHLATER",
+              payload: videoInfo,
+            })
+          }
+        >
+          Save to watch Later
+        </p>
         <p>Save to liked one</p>
       </div>
     </span>
