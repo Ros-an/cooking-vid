8b03e565c175ce5d8cbbaf4f99bc4e19da9e1a4b
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-15 05:01:19 +0530

    feat: conditonal rendering header item

--- src/Shared/Header.js
+++ src/Shared/Header.js
@@ -1,16 +1,23 @@
 import React from "react";
 import PlayCircleFilledIcon from "@material-ui/icons/PlayCircleFilled";
-import SearchIcon from "@material-ui/icons/Search";
-import AccountCircleRoundedIcon from "@material-ui/icons/AccountCircleRounded";
 import MenuIcon from "@material-ui/icons/Menu";
-import { NavLink } from "react-router-dom";
-import { useAuthContext } from "../ContextAPI/auth-context";
+import SignOut from "../auth/SignOut";
+import { isAuthenticated } from "../utils/authrelated";
+import { NavLink, useNavigate, useLocation } from "react-router-dom";
+import { useAuthContext } from "../context/auth-context";
+import { useFilterContext } from "../context/filterContext";
 import "./Header.css";
 
 function Header() {
   const { authPage, slideOpen, slideClose } = useAuthContext();
+  const {
+    filters: { text },
+    updateFilters,
+  } = useFilterContext();
+  let navigate = useNavigate();
+  let location = useLocation();
   return (
-    <div className="header">
+    <div className="header" style={{ background: "white" }}>
       <span
         className={`menu-icon ${
           authPage ? "rotate-clock" : "rotate-anticlock"
@@ -27,19 +34,33 @@ function Header() {
           <PlayCircleFilledIcon style={{ fontSize: "1.5rem" }} />
         </button>
       </NavLink>
-      <div className="search-section">
-        <input type="text" placeholder="Search" />
-        <button className="search-icon">
-          <SearchIcon className="search-icon" />
-        </button>
+      <div
+        className="search-section"
+        style={{
+          visibility: location.pathname === "/" ? "visible" : "hidden",
+        }}
+      >
+        <input
+          type="text"
+          name="text"
+          value={text}
+          placeholder="search"
+          onChange={updateFilters}
+        />
       </div>
       <NavLink to="/authenticate">
         <p className="login">
           {" "}
-          <span className="login-item__text">Login</span>
-          <span className="login-item__logo">
-            <AccountCircleRoundedIcon style={{ fontSize: "1.75rem" }} />
-          </span>
+          {isAuthenticated() ? (
+            <SignOut />
+          ) : (
+            <span
+              className="login-item__text"
+              onClick={() => navigate(`/authenticate`)}
+            >
+              Signin
+            </span>
+          )}
         </p>
       </NavLink>
     </div>
