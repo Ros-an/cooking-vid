220ad07e372decaa16c120e3d848259d88c4e25c
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-15 04:58:06 +0530

    refactor: logic and removed few

--- src/Reducer/playlist-reducer.js
+++ src/Reducer/playlist-reducer.js
@@ -7,50 +7,19 @@ export const reducer = (state, action) => {
         singleVideo: state.singleVideo === "" ? action.payload : "",
         playListModalState: state.playListModalState ? false : true,
       };
-    // case "INPUT_BOX_STATE":
-    //   return {
-    //     ...state,
-    //     playListInputBox: state.playListInputBox ? false : true,
-    //   };
-    // playListInputBox: state.playListInputBox ? false : true,
+    case "ADD_TO_PLAYLIST":
+      return {
+        ...state,
+        playListStorage: [...action.payload],
+      };
+
     case "SAVE_NEW_PLAYLIST":
       return {
         ...state,
-        playListStorage: [
-          ...state.playListStorage,
-          {
-            id: new Date().getTime().toString(),
-            name: action.payload,
-            videoList: [],
-          },
-        ],
-      };
-    case "TOGGLING_VID_CHECKBOX":
-      const { payload } = action;
-      return {
-        ...state,
-        playListStorage: payload,
+        playListStorage: [...action.payload],
       };
 
-    case "REMOVE_VID_FROM_PLAYLIST":
-      const {
-        payload: { id, playListId },
-      } = action;
-      const reqPlayList = state.playListStorage.find(
-        (list) => list.id === playListId
-      );
-      const reqPlayListUpdated = reqPlayList.videoList.filter(
-        (vid) => vid.id !== id
-      );
-      const updatedPlaylistStorage = state.playListStorage.map((list) =>
-        list.id === playListId
-          ? { ...reqPlayList, videoList: reqPlayListUpdated }
-          : list
-      );
-      return {
-        ...state,
-        playListStorage: updatedPlaylistStorage,
-      };
+    
     default:
       return state;
   }
