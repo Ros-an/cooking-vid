3fb5de822681414fdd3e0385fe067fba6de0fb53
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-15 04:57:15 +0530

    refactor : logic  and moved form swich to if statement

--- src/Reducer/likeHistoryWatchLater-reducer.js
+++ src/Reducer/likeHistoryWatchLater-reducer.js
@@ -1,60 +1,31 @@
 export const reducer = (state, action) => {
   const { type, payload } = action;
-  switch (type) {
-    case "ADD_TO_WATCHLATER":
-      const presentOrNot1 = state.watchLater.some(
-        (vid) => vid.id === payload.id
-      );
-      if (presentOrNot1) {
-        return { ...state };
-      }
-      return { ...state, watchLater: [...state.watchLater, payload] };
-
-    case "REMOVE_FROM_WATCHLATER":
-      const newWatchLater = state.watchLater.filter(
-        (vid) => vid.id !== payload.id
-      );
-      return { ...state, watchLater: newWatchLater };
-
-    case "ADD_TO_HISTORY":
-      const newHistory = state.history.filter((vid) => vid.id !== payload.id);
-      return {
-        ...state,
-        history: [payload, ...newHistory],
-      };
-
-    case "REMOVE_FROM_HISTORY":
-      const newHistory1 = state.history.filter((vid) => vid.id !== payload.id);
-      return {
-        ...state,
-        history: newHistory1,
-      };
-
-    case "TOGGLE_LIKE":
-      const presentOrNot2 = state.likedOne.some((vid) => vid.id === payload.id);
-      if (presentOrNot2) {
-        const newLikedVid1 = state.likedOne.filter(
-          (vid) => vid.id !== payload.id
-        );
-        return {
-          ...state,
-          likedOne: newLikedVid1,
-        };
-      }
-      return {
-        ...state,
-        likedOne: [...state.likedOne, payload],
-      };
-
-    case "REMOVE_FROM_LIKEDVID":
-      const newLikedVid2 = state.likedOne.filter(
-        (vid) => vid.id !== payload.id
-      );
-      return {
-        ...state,
-        likedOne: newLikedVid2,
-      };
-    default:
-      return state;
+  if (type === "UPDATE_DEFAULTSTATE_ARRAY") {
+    return {
+      ...state,
+      watchLater: payload.watchLater,
+      likedVideo: payload.likedVideo,
+      history: payload.history,
+    };
   }
+  if (type === "UPDATE_HISTORY") {
+    return {
+      ...state,
+      history: payload,
+    };
+  }
+  if (type === "UPDATE_WATCHLATER") {
+    return {
+      ...state,
+      watchLater: payload,
+    };
+  }
+  if (type === "UPDATE_LIKEDVIDEO") {
+    return {
+      ...state,
+      likedVideo: payload,
+    };
+  }
+
+  throw new Error(`No Matching "${type}" - action type`);
 };
