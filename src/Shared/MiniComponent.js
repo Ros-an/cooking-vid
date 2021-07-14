13d22b72c7a9372db5ad8fe69164a21d505c039d
Roshan Kr. Mahato <76640700+Ros-an@users.noreply.github.com>
2021-07-15 05:03:09 +0530

    refactor: structure and feat: remove

--- src/Shared/MiniComponent.js
+++ src/Shared/MiniComponent.js
@@ -1,20 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./MiniComponent.css";
-
-function NoItem({ heading, buttonText }) {
-  return (
-    <article className="no-item">
-      <p>{heading}</p>
-      <button>{buttonText}</button>
-    </article>
-  );
-}
-function SectionHeading({ headingName }) {
-  return <h2 className="section-heading">{headingName}</h2>;
-}
+import {
+  removeFromLikedVideo,
+  removeFromWatchLater,
+  removeFromHistory,
+} from "../api/like_watch_history_api";
+import { removeVideo } from "../api/playlist_api";
+import { userInfo } from "../utils/authrelated";
+import { useNavigate } from "react-router-dom";
 
 function VideoCardTypeList({
-  id,
+  _id,
   image,
   title,
   views,
@@ -24,28 +20,69 @@ function VideoCardTypeList({
   remove,
   playListId,
 }) {
-  console.log(remove);
+  const [loader, setLoader] = useState(false);
+  let navigate = useNavigate();
+  const removeFunction = () => {
+    const userId = userInfo()?.user?._id;
+    if (remove === "REMOVE_FROM_LIKEDVID") {
+      removeFromLikedVideo(userId, _id, dispatch, setLoader);
+    }
+    if (remove === "REMOVE_FROM_HISTORY") {
+      removeFromHistory(userId, _id, dispatch, setLoader);
+    }
+    if (remove === "REMOVE_FROM_WATCHLATER") {
+      removeFromWatchLater(userId, _id, dispatch, setLoader);
+    }
+    if (remove === "REMOVE_VID_FROM_PLAYLIST") {
+      removeVideo({
+        playlistId: playListId,
+        videoId: _id,
+        userId,
+        dispatchPlayList: dispatch,
+        setLoaderTwo: setLoader,
+      });
+    }
+  };
+
+  const navigation = () => {
+    return navigate(`/${_id}`);
+  };
   return (
     <article className="playlist-video-card">
-      <img src={image} alt={title} />
+      <img
+        src={image}
+        alt={title}
+        onClick={navigation}
+        className="pointer-cursor"
+      />
       <div className="details">
-        <h3>{title}</h3>
+        <h3 className="pointer-cursor" onClick={navigation}>
+          {title}
+        </h3>
         <p>Channel : {channel}</p>
         <p>{views}</p>
         <p>{timestamp}</p>
-        <span
-          className="pointer-cursor"
-          onClick={() =>
-            dispatch({
-              type: remove,
-              payload: { id, playListId },
-            })
-          }
-        >
-          remove
+        <span className="pointer-cursor" onClick={removeFunction}>
+          {loader ? "removing..." : "remove"}
         </span>
       </div>
     </article>
   );
 }
+
+// Page incase no item
+function NoItem({ heading, buttonText }) {
+  let navigate = useNavigate();
+  return (
+    <article className="no-item">
+      <p>{heading}</p>
+      <button onClick={() => navigate(`/`)}>{buttonText}</button>
+    </article>
+  );
+}
+// Heading
+function SectionHeading({ headingName }) {
+  return <h2 className="section-heading">{headingName}</h2>;
+}
+
 export { NoItem, SectionHeading, VideoCardTypeList };
